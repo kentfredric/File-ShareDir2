3286ebe9a0c5c21f4c2f5dd71e59f122b80be1c5
Kent Fredric <kentfredric@gmail.com>
2015-07-12 19:16:57 +1200

    Build results of a58404d (on master)

--- lib/File/ShareDir2.pm
+++ lib/File/ShareDir2.pm
@@ -791,7 +791,7 @@ Tatsuhiko Miyagawa <miyagawa@cpan.org>
 
 =head1 COPYRIGHT AND LICENSE
 
-This software is copyright (c) 2011 by Adam Kennedy <adamk@cpan.org>.
+This software is copyright (c) 2005 by Adam Kennedy <adamk@cpan.org>.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
